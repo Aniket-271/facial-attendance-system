83631bd8aa313b4da7d3b4dd7fe855666e0eab10
Aniket-271 <aniketverma2603@gmail.com>
2024-09-03 20:28:54 +0530

    Update EncodeGenerator.py

--- EncodeGenerator.py
+++ EncodeGenerator.py
@@ -2,23 +2,7 @@ import cv2
 import face_recognition
 import pickle
 import os
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import db
-from firebase_admin import storage
 
-cred = credentials.Certificate("serviceAccountKey.json")
-firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://facial-attendance-system-a6183-default-rtdb.firebaseio.com/',
-    'storageBucket': 'facial-attendance-system-a6183.appspot.com'
-})
-
-# Importing student images
-folderPath = 'Images'
-pathList = os.listdir(folderPath)
-print(pathList)
-imgList = []
-studentIds = []
 
 for path in pathList:
     imgList.append(cv2.imread(os.path.join(folderPath, path)))
